2b7fb4669eb66e50b448703f79c39ecb73ab0c2f
ARHITEKTOR <danilov@mail.de>
2023-12-09 21:37:33 +0100

    Zugriff auf den Ordner und durchnummerierte Ausgabe der Dateien im Ordner

--- main.py
+++ main.py
@@ -7,13 +7,30 @@ pfad = "C:\\Users\\ARHITEKTOR\\Desktop\\BERICHTSHEFT"
 # Dateien in im angegeben Ordner durchlaufen und in der Liste Speichern
 file_liste = os.listdir(pfad)
 
-# Durchlaufe und nummeriere die Dateien in der Liste durch
-for index, name in enumerate(file_liste):
-    # Die Nummerierung soll mit einer 1 beginnen, anstatt mit 0.
-    index = index + 1
 
-    # Führende Null hinzufügen, wenn der Index nur eine Ziffer hat
-    if len(str(index)) == 1:
-        index = f"0{index}"
-    # Durchnummerierte Ausgabe der Dateien
-    print("Nummer:", index, " | ", "Dateiname:", name)
+def ausgabe():
+    # Durchlaufe und nummeriere die Dateien in der Liste durch
+    for name in file_liste:
+        # Durchnummerierte Ausgabe der Dateien
+        print("Dateiname: | ", name)
+
+
+def umbenennen():
+    # Durchlaufe und nummeriere die Dateien in der Liste durch
+    for index, name in enumerate(file_liste):
+        # Die Nummerierung soll mit einer 1 beginnen, anstatt mit 0.
+        index = index + 1
+
+        # Führende Null hinzufügen, wenn der Index nur eine Ziffer hat
+        if len(str(index)) == 1:
+            index = f"0{index}"
+
+        # Umbenennung der Dateien
+        os.rename(os.path.join(pfad, name), os.path.join(pfad, f"{index}_{name}"))
+
+        # Durchnummerierte Ausgabe der Dateien
+        print("Nr.:", index, " | ", "Dateiname:", name)
+
+
+ausgabe()
+# umbenennen()
